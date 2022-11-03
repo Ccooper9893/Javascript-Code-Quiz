830a5f71858f9b9268e2fae2c25b49d8d009adf0
Cody Cooper <cody_codster@live.com>
2022-11-02 20:03:10 -0500

    Added localStorage objects to Javascript

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,9 @@
 //Step 4: Retrieve previous highscores to store in local storage
 //Step 5: Create functions
 //Step 6: Create Event Listers
+//Step 7: Save scores and initials
 
+//-----------------------Create variables that are selected html elements-------------------------
 //Header elements
 var highscoresLink = document.querySelector('#highscores');
 var timerLink = document.querySelector('#timer');
@@ -22,6 +24,7 @@ var answer1 = document.querySelector('#answer1');
 var answer2 = document.querySelector('#answer2');
 var answer3 = document.querySelector('#answer3');
 var answer4 = document.querySelector('#answer4');
+var answerArr = [answer1, answer2, answer3, answer4]
 var tempResult = document.querySelector('#result');
 
 //Score Result Page
@@ -34,39 +37,50 @@ var submitButton = document.querySelector('#submit-button');
 var highscoresMenu = document.querySelector('#highscores-page');
 var highscoreList = document.querySelector('#saved-highscores');
 var resetQuizButton = document.querySelector('#reset-button');
-
-//Sets the visibility of the pages
-questionMenu.style.display = 'none';
-highscoresMenu.style.display = 'none';
-submitMenu.style.display = 'none';
-
-//Set timer
-var secondsLeft = 100;
-
-function setTime() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timeLeft.textContent = secondsLeft;
-
-        if (secondsLeft === 0) {
-            
-        }
-    })
-}
+var highscoreButton = document.querySelector('#highscore-btn');
 
 //Score
-var currentScore = 0; //
+var currentScore = 0; //Creates a score variable
 var scoreList = []; //Makes an array for saved scores
+
+//Question number index
+var currentQuestion = 0;
 
 //Initals
 var initialList = [];
 
-//Sets the visibility of the pages
+//Link to highscore page
+highscoreButton.addEventListener('click', highscoreLink);
+
+//--------------------Sets the visibility of the pages-------------------------
 questionMenu.style.display = 'none';
 highscoresMenu.style.display = 'none';
 submitMenu.style.display = 'none';
 
-//Start Game function
+//----------------------------------Set timer-----------------------------------
+var secondsLeft = 0
+
+function setTime() {
+    secondsLeft = 100;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft;
+
+        if (secondsLeft <= 0) {
+            submitMenu.style.display = '';
+            questionMenu.style.display = 'none';
+            clearInterval(timerInterval);
+        } 
+    }, 1000)
+}
+
+
+//-----------------------Sets the visibility of the pages----------------
+questionMenu.style.display = 'none';
+highscoresMenu.style.display = 'none';
+submitMenu.style.display = 'none';
+
+//-------------------Start Game functionality ---------------------------
 var startGame = function () {
 
     //Starts timer
@@ -77,80 +91,195 @@ var startGame = function () {
     questionMenu.style.display = '';
     
     //Inserts the first question and answers into the element
-    questionText.textContent = question1.question;
+    questionText.textContent = questions[0].question;
 
     //Inserts answers into buttons
-    answer1.textContent = question1.answers[0];
-    answer2.textContent = question1.answers[1];
-    answer3.textContent = question1.answers[2];
-    answer4.textContent = question1.answers[3];
+    for (i=0; i<4; i++) {
+        answerArr[i].textContent = questions[currentQuestion].answers[i];
+    }
 
-    //
 }
 
+//Listens for start button to be clicked
 startButton.addEventListener('click', startGame);
 
 
+//------------------------Answer Selection functionaliy--------------------------
+
+
+
+//Listens for answers be clicked
+answer1.addEventListener('click', selectAnswer);
+answer2.addEventListener('click', selectAnswer);
+answer3.addEventListener('click', selectAnswer);
+answer4.addEventListener('click', selectAnswer);
+
+
+function selectAnswer(event) {
+    
+    //Checking if selected answer is correct/incorrect
+        if (event.target.innerText === questions[currentQuestion].correct) {
+            tempResult.textContent = 'Correct!'
+            currentScore = currentScore += 25;
+            console.log('Correct');
+            currentQuestion++;
+        } else {
+            tempResult.textContent = 'Incorrect!'
+            secondsLeft -= 10;
+            console.log('Incorrect');
+            currentQuestion++;
+        }
+
+         //Goes to next quiz question
+            nextQuestion();
+    }
+    
+   
+    
+
+
+
+//--------------------------Next Question---------------------------//
+function nextQuestion() {
+
+    if (currentQuestion === questions.length) {
+        //Ends game and calls gameOver function
+        gameOver();
+            
+    } else {
+        //Inserts the next question and answers into the element
+        questionText.textContent = questions[currentQuestion].question;
+
+        //Inserts answers into buttons
+        for (i=0; i<4; i++) {
+        answerArr[i].textContent = questions[currentQuestion].answers[i];
+    }
+    }
+}
+
+
+//-------------------------------  Gameover Page-------------------------------------------
+
+function gameOver() {
+    submitMenu.style.display = '';
+    questionMenu.style.display = 'none';
+
+    //Resets time
+    secondsLeft = 0;
+    timeLeft.textContent = secondsLeft;
+    //Displays score in submitMenu page.
+    scoreResult.textContent = currentScore;
+
+    //Pushes score to scoreList array.
+    scoreList.push(currentScore);
+
+    // initialFill.textContent = // --------------------------------------Need help saving initials/score to array and pushing  to local storage.
+
+    //Creates list items to display current and past saved scores
+}
+
+submitButton.addEventListener('click', highScorePage);
+
+//-------------------------------Highscores Page-------------------------------------------
+function highscoreLink () {
+    startMenu.style.display = 'none'
+    submitMenu.style.display = 'none';
+    highscoresMenu.style.display = '';
+    listHighcores();
+}
+
+function highScorePage () {
+    initialList.push(initialFill.value);
+    submitMenu.style.display = 'none';
+    highscoresMenu.style.display = '';
+    localStorage.setItem('Score:', currentScore);
+    localStorage.setItem('Initials:', initialFill.value);
+    
+    //Creates a list item
+    var li = document.createElement('li');
+
+    //Fill list element with Initials and score
+    li.innerText = `Player: ${initialFill.value} ---|--- Score: ${currentScore}`;
+
+    //Inserts li into highscoreList ul
+    highscoreList.append(li);       
+    }
+
+function listHighcores (e) {
+    
+}
+resetQuizButton.addEventListener('click', reset);
+
+
+//Resets the quiz
+function reset () {
+    questionMenu.style.display = 'none';
+    highscoresMenu.style.display = 'none';
+    submitMenu.style.display = 'none';
+    startMenu.style.display = '';
+    currentScore = 0;
+    currentQuestion = 0;
+}
+
+
+/* ------------------------------Questions/Answers-----------------------------------------*/
+
+var questions = [ 
+
+    { 
+        question: 'What method is used to print to the console?', 
+        answers: ['.push()', '.pop()', '.concat()', 'console.log()'], 
+            correct: 'console.log()'
+    },
+
+
+    { 
+    question: 'What statement creates a loop that executes as long as the condition is (!true)?', 
+    answers: ['for', 'do while', 'while', 'if else'],
+        correct: 'do while'
+
+    },
+
+
+    {
+        question:'What tag is used to link Javascript to HTML?',
+            answers: ['<link>', '<div>', '<script>', '<button>'], 
+                correct: '<script>'
+    },
 
 
 
 
-/* ------------------------------Questions-----------------------------------------*/
-var currentQuestion = 0;
+    {
+        question: 'Which one of these data types is a boolean value?', 
+            answers: ['null', 'string', 'false', 'bigInt'], 
+                correct: 'false'
 
-var question1 = {
-    question: 'What method is used to print to the console?',
-    answers: ['.push()', '.pop()', '.concat()', 'console.log()'],
-    correctAnswer: 'console.log()'
-};
-
-var question2 = {
-    question: 'What statement creates a loop that executes as long as the condition is (!true)?',
-    answers : ['for', 'do while', 'while', 'if else'],
-    correctAnswer: 'do while'
-};
-
-var question3 = {
-    question: 'What tag is used to link Javascript to HTML?',
-    answers : ['<link>', '<div>', '<script>', '<button>'],
-    correctAnswer: '<script>'
-};
-
-var question4 = {
-    question: 'Which one of these data types is a boolean value?',
-    answers : ['null', 'string', 'false', 'bigInt'],
-    correctAnswer: 'false'
-};
-
-var question5 = {
-    question: 'Which one is a DOM property that allows modifying element classes? ',
-    answers : ['.classList', '.appendChild', '.textContent', '.createElement'],
-    correctAnswer: '.classList'
-};
-
-var question6 = {
-    question: 'What are the possible types of events for the addEventLister method?',
-    answers : ['click', 'mousewheel', 'scroll', 'all of the above'],
-    correctAnswer: 'all of the above'
-};
-
-var question7 = {
-    question: 'Which of these methods will add an element to the beginning of an array?',
-    answers : ['push()', 'shift()', 'concat()', 'unshift()'],
-    correctAnswer: 'unshift()'
-};
-
-//Creates array of available questions
-var questionArr = [question1, question2, question3, question4, question5, question6, question7];
+    },
 
 
+    {
+        question: 'Which one is a DOM property that allows modifying element classes? ', 
+            answers:['.classList', '.appendChild', '.textContent', '.createElement'], 
+                correct: '.classList'
+
+    },
 
 
+    {
+        question: 'What are the possible types of events for the addEventLister method?', 
+            answers: ['click', 'mousewheel', 'scroll', 'all of the above'], 
+                correct: 'all of the above'
+
+    },  
 
 
-
-
-
+    {
+        question: 'Which of these methods will add an element to the beginning of an array?', 
+            answers: ['push()', 'shift()', 'concat()', 'unshift()'], 
+                correct: 'unshift()'
+    }
+]
 
 
 //------------------------------------Experimental Code -----------------------------------------------------
@@ -158,8 +287,8 @@ var questionArr = [question1, question2, question3, question4, question5, questi
 //Function will run when page loads.
 // function init() {
 //     // Get stored scores from localStorage
-//     var storedScores = JSON.parge(localStorage.getItem('scoreList'));
-//     var storedInitials = JSON.parge(localStorage.getItem('initalList'));
+//     var storedScores = JSON.parse(localStorage.getItem('scoreList'));
+//     var storedInitials = JSON.parse(localStorage.getItem('initalList'));
 
 //     // If there are stored scores updated score list
 //     if (storedScores !== null) {
@@ -173,15 +302,13 @@ var questionArr = [question1, question2, question3, question4, question5, questi
 // }
 
 //Listing the scores on the 
-// function listHighcores () {
-//     if (storedScores !== null && highscoresMenu.style.display !== 'none') {
+           // scoreList.forEach(function(e) {
+            //     initialList.forEach(function (e) {   
+            //     var li = document.createElement('li')
+            //     li.innerText = initial;
+            //     highscoreList.append(li)
+        
+            
+                 
 
-//         for (i=0; i<scoreList.length; i++) {
-//             var storedScores
-//             highscoreList.appendChild('li');
 
-
-
-//         }
-//     }
-// }
